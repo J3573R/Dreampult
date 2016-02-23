28e46d2295a6d233c6e6e21105c6035be2f6f4c6
zuwaiss <kalle.m.heinonen@gmail.com>
2016-02-23 20:47:15 +0200

    CHANGES: - Tile map added to GameLoop
    TO DO:
    - Decide on the scale for the map (if not already decided)
    - Use the scale mentioned above to correct the size of objects

--- core/src/fi/tamk/dreampult/GameLoop.java
+++ core/src/fi/tamk/dreampult/GameLoop.java
@@ -7,8 +7,17 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by Clown on 22.2.2016.
@@ -41,6 +50,28 @@ public class GameLoop extends ScreenAdapter {
     public boolean moveArrow;
 
     /**
+     * The size of the map is 1000 x 500 tiles.
+     */
+    int TILES_AMOUNT_WIDTH = 1000;
+    int TILES_AMOUNT_HEIGHT = 500;
+
+    /**
+     * The size of a single tile is 16 x 16.
+     */
+    int TILE_WIDTH = 16;
+    int TILE_HEIGHT = 16;
+
+    /**
+     * The tile map used by the game.
+     */
+    private TiledMap tiledMap;
+
+    /**
+     * Used for rendering the tile map.
+     */
+    private TiledMapRenderer tiledMapRenderer;
+
+    /**
      * Initialize variables for render.
      * @param game
      * @param camera
@@ -64,6 +95,15 @@ public class GameLoop extends ScreenAdapter {
         background.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
         fullBackground = new TextureRegion(background);
         fullBackground.setRegion(0, 0, background.getWidth() * (background.getWidth() / WORLD_WIDTH), background.getHeight());
+
+        tiledMap = new TmxMapLoader().load("mappi.tmx");
+
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1/100f);
+
+        transformWallsToBodies("object-maa", "wall");
+        transformWallsToBodies("object-paprika", "collectable");
+
+
     }
 
     /**
@@ -78,6 +118,10 @@ public class GameLoop extends ScreenAdapter {
         doPhysicsStep(delta);
         worldHandler.moveCamera();
         game.batch.setProjectionMatrix(camera.combined);
+
+        tiledMapRenderer.setView(camera);
+
+
         if(moveArrow) {
 
             if(arrow.rotation > 0) {
@@ -102,11 +146,19 @@ public class GameLoop extends ScreenAdapter {
          */
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        tiledMapRenderer.render();
+
         game.batch.begin();
-        game.batch.draw(fullBackground, 0, 0, 240, 5);
+
+        //game.batch.draw(fullBackground, 0, 0, 240, 5);
+
         arrow.draw(game.batch);
+
         player.draw(game.batch);
+
         game.batch.end();
+
         debug.render(world, camera.combined);
     }
 
@@ -129,5 +181,70 @@ public class GameLoop extends ScreenAdapter {
         }
     }
 
+    /**
+     * Takes a tiledmap layer and transforms it to a box2D Body.
+     * @param layer The layer to transform.
+     * @param userData The userData of the layer.
+     */
+    private void transformWallsToBodies(String layer, String userData) {
+        MapLayer collisionObjectLayer = tiledMap.getLayers().get(layer);
+
+        MapObjects mapObjects = collisionObjectLayer.getObjects();
+
+        Array<RectangleMapObject> rectangleObjects = mapObjects.getByType(RectangleMapObject.class);
+
+        for (RectangleMapObject rectangleObject : rectangleObjects) {
+            Rectangle tmp = rectangleObject.getRectangle();
+
+            Rectangle rectangle = scaleRect(tmp, 1 / 100f);
+
+            createStaticBody(rectangle, userData);
+        }
+    }
+
+    /**
+     * Creates a static body, and sets it userData.
+     * @param rect The rectangle to use as the basis.
+     * @param userData The userData to give the rectangle.
+     */
+    public void createStaticBody(Rectangle rect, String userData) {
+        BodyDef myBodyDef = new BodyDef();
+        myBodyDef.type = BodyDef.BodyType.StaticBody;
+
+        float x = rect.getX();
+        float y = rect.getY();
+        float width = rect.getWidth();
+        float height = rect.getHeight();
+
+        float centerX = width/2 + x;
+        float centerY = height/2 + y;
+
+        myBodyDef.position.set(centerX, centerY);
+
+        Body wall = world.createBody(myBodyDef);
+
+        wall.setUserData(userData);
+
+        PolygonShape groundBox = new PolygonShape();
+
+        groundBox.setAsBox(width / 2 , height / 2 );
+
+        wall.createFixture(groundBox, 0.0f);
+    }
+
+    /**
+     * Scales a rectangle to the desired scale.
+     * @param r The rectangle to scale.
+     * @param scale Used for scaling the rectangle.
+     * @return Returns a properly scaled rectangle.
+     */
+    private Rectangle scaleRect(Rectangle r, float scale) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.x         = r.x * scale;
+        rectangle.y         = r.y * scale;
+        rectangle.width     = r.width * scale;
+        rectangle.height    = r.height * scale;
+        return rectangle;
+    }
 
 }
